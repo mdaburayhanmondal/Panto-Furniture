7fd4e949d98f6bba4a7317228309b71b2b135dd9
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-01-08 20:11:02 +0600

    added Contact Us page

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import contactBackground from '../../src/assets/contact-background.jpg';
+import Experience from './home/Experience';
 
 const ContactUs = () => {
-  return <div>ContactUs</div>;
+  return (
+    <section>
+      <div
+        className="w-full h-100 bg-no-repeat bg-center bg-cover flex item justify-center text-white"
+        style={{ backgroundImage: `url(${contactBackground})` }}
+      >
+        <div class="mt-4 h-fit bg-white/10 backdrop-blur-sm border border-white/20 rounded-xl p-4">
+          <h1 class="text-5xl font-bold text-white">Contact Us</h1>
+        </div>
+      </div>
+      <Experience />
+    </section>
+  );
 };
 
 export default ContactUs;
