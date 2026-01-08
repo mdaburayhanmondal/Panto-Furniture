674293134fd30983573b8e3b44406136c46d682d
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-01-08 12:33:56 +0600

    changed button style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,9 +3,13 @@ import buttonArray from '../assets/button-icon.png';
 
 const Button = ({ buttonText }) => {
   return (
-    <button className="text-sm text-primary flex items-center gap-x-1 cursor-pointer">
+    <button className="w-fit text-sm text-primary flex items-center cursor-pointer hover:-translate-y-2 transition-all duration-300 ease-in group underline underline-offset-4 hover:decoration-transparent px-2 py-1">
       {buttonText}
-      <img src={buttonArray} alt="" />
+      <img
+        src={buttonArray}
+        alt=""
+        className="group-hover:translate-x-1 hover:translate-y-0 transition-all duration-300 ease-in opacity-0 -translate-x-8 group-hover:opacity-100"
+      />
     </button>
   );
 };
