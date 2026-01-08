2fae53d3943f5e1606e130f38d7f1434699159d3
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-01-08 20:10:52 +0600

    added About Us page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import aboutBackground from '../../src/assets/about-background.jpg';
+import Experience from './home/Experience';
 
 const AboutUs = () => {
-  return <div>AboutUs</div>;
+  return (
+    <section>
+      <div
+        className="w-full h-100 bg-no-repeat bg-center bg-cover flex item justify-center text-white"
+        style={{ backgroundImage: `url(${aboutBackground})` }}
+      >
+        <div class="mt-4 h-fit bg-white/10 backdrop-blur-sm border border-white/20 rounded-xl p-4">
+          <h1 class="text-5xl font-bold text-white">About Us</h1>
+        </div>
+      </div>
+      <Experience />
+    </section>
+  );
 };
 
 export default AboutUs;
