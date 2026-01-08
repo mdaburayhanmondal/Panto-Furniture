47ae31cd9a1de79d75db15220f10d530b780a7b3
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-01-08 21:49:45 +0600

    wraped with ThemeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,20 @@ import { Outlet, useLocation } from 'react-router';
 import './App.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import { ThemeProvider } from './context/ThemeContext';
 
 function App() {
   const location = useLocation();
   const isHomePage = location.pathname === '/';
 
   return (
-    <>
+    <ThemeProvider>
       <Navbar />
       <main className={`min-h-screen ${isHomePage ? 'pt-0' : 'pt-14'}`}>
         <Outlet />
       </main>
       <Footer />
-    </>
+    </ThemeProvider>
   );
 }
 
