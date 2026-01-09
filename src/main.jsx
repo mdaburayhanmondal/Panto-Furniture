b766aa94f2e3f44321875bc0d19a1318d089d1a0
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-01-09 16:26:24 +0600

    changed declarative style to data router style

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,49 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import { ThemeProvider } from './context/ThemeContext.jsx';
-import { BrowserRouter, Route, Routes } from 'react-router';
+
 import Home from './pages/home/Home.jsx';
 import Shop from './pages/shop/Shop.jsx';
 import AboutUs from './pages/AboutUs.jsx';
 import ContactUs from './pages/ContactUs.jsx';
 import Cart from './pages/Cart.jsx';
-import Swal from 'sweetalert2';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: 'shop',
+        element: <Shop />,
+      },
+      {
+        path: 'about',
+        element: <AboutUs />,
+      },
+      {
+        path: 'contact',
+        element: <ContactUs />,
+      },
+      {
+        path: 'cart',
+        element: <Cart />,
+      },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
-  <ThemeProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route element={<App />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/cart" element={<Cart />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </ThemeProvider>
+  <StrictMode>
+    <ThemeProvider>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </StrictMode>
 );
